7f2471a7ef3020167eb04fe2f2ee287ea5cebbe6
ViskyKrisztina <visky.krisztina@yahoo.com>
2021-06-14 16:09:55 +0300

    implemented firebase utils, included ability to store authenticated users into our firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,25 +7,28 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 
 const Header = ({ currentUser }) => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
-            </Link>
-            {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
 export default Header;
